b75b5902e9947af99592d7a221ddf86b08999c82
sonjhalyns augustin <sonjhalynsaugustin@gmail.com>
2023-04-28 21:33:12 -0400

    Initial Commit

---
+++ finalbudget/finalbudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalbudgetApp.swift
+//  finalbudget
+//
+//  Created by Sonjhalyns Augustin on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct finalbudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
